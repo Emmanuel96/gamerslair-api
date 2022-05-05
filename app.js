f43328c18490a5e750702ed1588761f4f91bc154
Emmanuel96 <emmanuel.audu1@aun.edu.ng>
2022-05-05 17:25:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+
+const DB = require("./database/mongodb")
+const indexRouter = require('./routes/index')
+const middleware = require('./utils/middleware')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+app.use(morgan('tiny'))
+
+app.use(indexRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
