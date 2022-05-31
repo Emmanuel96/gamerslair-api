914c49a9e7039a4367ab626cc99f4a1495daf6c2
Emmanuel96 <emmanuel.audu1@aun.edu.ng>
2022-05-31 05:11:08 +0100

    stash

--- routes/challenge.js
+++ routes/challenge.js
@@ -1,14 +1,15 @@
 const express = require("express")
 const Router = express.Router()
+const authenticateToken = require('../middlewares/jwt').authenticateToken
 
 const ChallengeController = require('../controllers/ChallengeController')
 
 // Router.get('/api/challenges/')
-Router.post('/api/challenge/create', ChallengeController.create)
-Router.get('/api/challenge/fetch_all', ChallengeController.fetchAll)
-Router.get('/api/challenge/fetch/:id', ChallengeController.fetchOne)
-Router.get('/api/challenge/fetch_incoming/:user_id', ChallengeController.fetchIncoming)
-Router.get('/api/challenge/fetch_outgoing/:user_id', ChallengeController.fetchOutgoing)
-Router.put('/api/challenge/update/:id', ChallengeController.update)
-Router.delete('/api/challenge/delete/:id', ChallengeController.delete)
+Router.post('/api/challenge/create', authenticateToken, ChallengeController.create)
+Router.get('/api/challenge/fetch_all', authenticateToken, ChallengeController.fetchAll)
+Router.get('/api/challenge/fetch/:id', authenticateToken, ChallengeController.fetchOne)
+Router.get('/api/challenge/fetch_incoming/:user_id', authenticateToken, ChallengeController.fetchIncoming)
+Router.get('/api/challenge/fetch_outgoing/:user_id', authenticateToken, ChallengeController.fetchOutgoing)
+Router.put('/api/challenge/update/:id', authenticateToken, ChallengeController.update)
+Router.delete('/api/challenge/delete/:id', authenticateToken, ChallengeController.delete)
 module.exports = Router
