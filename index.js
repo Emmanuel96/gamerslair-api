c9887cbd36f5811580f1049544e60752047561cf
Emmanuel96 <emmanuel.audu1@aun.edu.ng>
2022-06-20 20:54:28 +0100

    added socket sessions

--- index.js
+++ index.js
@@ -9,12 +9,27 @@ const io = new Server(server);
 
 io.use(socketManager)
 
-io.on('connection', function (socket){
-  socket.join(socket.userID)
+io.on('connection', async function (socket){
+  const roomSockets = await io.in(socket.userID).fetchSockets()
+  const roomSocketsSessionIDs = roomSockets.map((soc)=> soc.sessionID)
+  if(roomSocketsSessionIDs.indexOf(socket.sessionID) < 0){
+    socket.join(socket.userID)
+  }
   console.log(`+user ${socket.userID} connected`);
   io.emit('new-event');
+
+  // emit session details
+  socket.emit("session", {
+    sessionID: socket.sessionID,
+  });
+
+  const matchingSockets = await io.in(socket.userID).allSockets();
+  console.log(matchingSockets)
 })
 
+io.on('disconnect', function(socket){
+  console.log(`+user ${socket.userID} disconnected`);
+})
 app.io = io
 
 server.listen(config.PORT, () => {
--- utils/socket.js
+++ utils/socket.js
@@ -1,22 +1,21 @@
+const crypto = require("crypto");
+const randomId = () => crypto.randomBytes(8).toString("hex");
+
 const socketManager = (socket, next) => {
-//   const sessionID = socket.handshake.auth.sessionID;
-//   if (sessionID) {
-//     // find existing session
-//     const session = sessionStore.findSession(sessionID);
-//     if (session) {
-//       socket.sessionID = sessionID;
-//       socket.userID = session.userID;
-//       return next();
-//     }
-//   }
-  const userID = socket.handshake.auth.userId;
-//   if (!userID) {
-//     return next(new Error("invalid userId"));
-//   }
-  // create new session
-//   socket.sessionID = randomId();
-  socket.userID = userID;
-  next();
+    const userID = socket.handshake.auth.userId;
+    if (!userID) {
+        return next(new Error("invalid userId"));
+    }
+    socket.userID = userID
+    const sessionID = socket.handshake.auth.sessionID;
+    if (sessionID) {
+        socket.sessionID = sessionID;
+        return next();
+    }
+    
+    socket.sessionID = randomId();
+    console.log(socket.sessionID)
+    next();
 }
 
 module.exports.socketManager = socketManager
